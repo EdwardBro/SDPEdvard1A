45f7df17bcab255358cea0c729752e903fce058f
Edward <edvard.brovkin@hva.nl>
2019-09-11 12:40:28 +0300

    Add project files.

---
+++ SDPEdvard1A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDPEdvard1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
