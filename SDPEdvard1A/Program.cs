1f6af6b9db4741d747714c699ab7bdddbf4ffbf7
Kuivalainen <54420379+DenisKuivalainen@users.noreply.github.com>
2019-09-28 22:35:46 +0300

    Update Program.cs

--- SDPEdvard1A/Program.cs
+++ SDPEdvard1A/Program.cs
@@ -7,7 +7,6 @@ namespace SDPEdvard1A
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is the first commit to repo!");
-        }
+            Console.WriteLine("This is the first commit to repo!");        }
     }
 }
